0035979180cdc81fe98ac43265fb9bd038355029
MaiaraMoro <maimoro98@gmail.com>
2023-08-25 21:32:20 -0300

    updated

--- Pipeline/Scripts_1/import_orthologs.py
+++ Pipeline/Scripts_1/import_orthologs.py
@@ -2,11 +2,14 @@ import csv
 import requests
 import time
 
-#function to fetch mammalian orthologs sequences using Ensembl API REST 
-def fetch_orthologs (ensembl_id):
+# function to fetch mammalian orthologs sequences using Ensembl API REST
+
+
+def fetch_orthologs(ensembl_id):
     server = "https://rest.ensembl.org"
     mammalian_target_taxon = "40674"
-    ext = f"/homology/id/{ensembl_id}?type=orthologues;target_taxon={mammalian_target_taxon};"
+    type_of_homology = "orthologues"
+    ext = f"/homology/id/{ensembl_id}?target_taxon={mammalian_target_taxon};type={type_of_homology}"
     headers = {"Content-Type": "application/json"}
 
     response = requests.get(server + ext, headers=headers)
@@ -15,61 +18,65 @@ def fetch_orthologs (ensembl_id):
         print("Request gone wrong!!!")
         response.raise_for_status()
         return
-    
+
     decoded = response.json()
     mammalian_orthologs = []
 
     for homology in decoded.get("data", [])[0].get("homologies", []):
         target = homology.get("target", {})
-        species = target.get("species", "")
-
-    mammalian_orthologs.append({
-        "Gene_Ensembl_ID": ensembl_id,
-        "Ortholog_Species": species,
-        "Ortholog_Protein_ID": target.get("protein_id"),
-        "Percentage_ID": homology.get("perc_id"),
-        "Percentage_Pos": homology.get("perc_pos")
-    })
+        mammalian_orthologs.append({
+            "Gene_Ensembl_ID": ensembl_id,
+            "Ortholog_Species": target.get("species", ""),
+            "Ortholog_Protein_ID": target.get("protein_id"),
+            "Percentage_ID": target.get("perc_id"),
+            "Percentage_Pos": target.get("perc_pos")
+        })
 
     return mammalian_orthologs
 
-#Load the list of genes from the csv file - obs the column with ensembl IDs must be called `ensembl`
-gene_list = [] 
+
+# Load the list of genes from the csv file - obs the column with ensembl IDs must be called `ensembl`
+gene_list = []
 with open("/home/mra/Downloads/Analises-masters/Pipeline/Scripts_1/InnateDB_TNFA.csv", "r") as readfile:
     reader = csv.DictReader(readfile)
     gene_list = [row['ensembl'] for row in reader]
+    print(gene_list)
 
 # writing in the new csv file
-with open('/home/mra/Downloads/Analises-masters/Pipeline/Scripts_1/genes.csv', 'w', newline='') as write_file:
-    fieldnames = ['Gene_Ensembl_ID', 'Ortholog_Species', 'Ortholog_Protein_ID', 'Percentage_ID', 'Percentage_Pos']
-    writer = csv.DictWriter(write_file, fieldnames=fieldnames)
-    writer.writeheader()
-    reqs_per_sec = 15
-    req_count = 0
-    last_req = 0
+for ensembl_id in gene_list:
+    with open(f"/home/mra/Downloads/Analises-masters/Pipeline/Scripts_1/{ensembl_id}.csv", 'w', newline='') as write_file:
+        fieldnames = ['Gene_Ensembl_ID', 'Ortholog_Species',
+                      'Ortholog_Protein_ID', 'Percentage_ID', 'Percentage_Pos']
+        writer = csv.DictWriter(write_file, fieldnames=fieldnames)
+        writer.writeheader()
+        reqs_per_sec = 15
+        req_count = 0
+        last_req = 0
 
-    for ensembl_id in gene_list:        # check if we need to rate limit ourselves
-        print(ensembl_id)
+        for ensembl_id in gene_list:        # check if we need to rate limit ourselves
+            print(ensembl_id)
+            finished_with_success = False
 
-        if req_count >= reqs_per_sec:
-            delta = time.time() - last_req
-            if delta < 1:
-                print("Maximum request limit reached!\n")
-                time.sleep(1 - delta)
-            last_req = time.time()
-            req_count = 0
-   
-        try:
-            orthologs = fetch_orthologs(ensembl_id) #function to call orthologs
-            print(orthologs)
-            if orthologs: #verify if the list is empty
-                writer.writerows(orthologs)
-                exit()
-            
-            req_count += 1
-            print(f"Finish processing :{ensembl_id}\n")
-        
-        except requests.exceptions.HTTPError as e:
-            print(f"Erro ao fazer solicitacao para {ensembl_id}:{e}")
-            time.sleep(10) #wait 10s until try again
-                         
+            while not finished_with_success:
+                if req_count >= reqs_per_sec:
+                    delta = time.time() - last_req
+                    if delta < 1:
+                        print("Maximum request limit reached!\n")
+                        time.sleep(1 - delta)
+                    last_req = time.time()
+                    req_count = 0
+
+                try:
+                    # function to call orthologs
+                    orthologs = fetch_orthologs(ensembl_id)
+                    print(orthologs)
+                    if orthologs:  # verify if the list is empty
+                        writer.writerows(orthologs)
+
+                    req_count += 1
+                    finished_with_success = True
+                    print(f"Finish processing :{ensembl_id}\n")
+
+                except requests.exceptions.HTTPError as e:
+                    print(f"Erro ao fazer solicitacao para {ensembl_id}:{e}")
+                    time.sleep(1)  # wait 10s until try again
